46af33412374e04640a46b480ef95bff21953800
Andresen <eric@andresen.codes>
2021-01-27 21:39:17 +0100

    Changed the Knight to Jack.

--- deck.go
+++ deck.go
@@ -8,7 +8,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Knight", "Queen", "King"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suits := range cardSuits {
 		for _, value := range cardValues {
@@ -21,7 +21,7 @@ func newDeck() deck {
 
 func (d deck) print() {
 	for i, card := range d {
-		fmt.Println(i, card)
+		fmt.Println(i+1, card)
 	}
 }
 
