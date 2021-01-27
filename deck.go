dc7f7d5d78e7591bcfaef3940369cf1574aeb4c9
Andresen <eric@andresen.codes>
2021-01-27 21:10:40 +0100

    Added deal function.

--- deck.go
+++ deck.go
@@ -5,22 +5,26 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
-		cards := deck{}
+	cards := deck{}
 
-		cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-		cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
 
-		for _, suits := range cardSuits {
-			for _, value := range cardValues {
-				cards = append(cards, value + " of " + suits)
-			}
+	for _, suits := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suits)
 		}
+	}
 
-		return cards
+	return cards
 }
 
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
